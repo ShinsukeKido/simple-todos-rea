b616967d608b836f8c00e08c30ef5686a3afe5b5
Shinsuke Kido <downmap555@gmail.com>
2018-07-25 10:30:02 +0900

    アプリ完成

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import TodoItem from './components/TodoItem';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import { index } from './api';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+    };
+    this.todosUpdater = this.todosUpdater.bind(this);
+  }
+
+  componentDidMount() {
+    this.todosUpdater();
+  }
+
+  // API から取得してステートを更新するメソッド
+  todosUpdater() {
+    index().then(response => this.setState({ todos: response }));
+  }
+
   render() {
     return (
       <div className="app">
-        <h3>Todo List</h3>
-        {/* Props を使うことで外から値が渡せる（関数の引数に似ている） */}
-        <TodoItem
-          title="Todo1のタイトル"
-          text="Todo1のテキスト"
-          created_at="Todo1の作成日時"
-        />
-        <TodoItem
-          title="Todo2のタイトル"
-          text="Todo2のテキスト"
-          created_at="Todo2の作成日時"
-        />
+        <TodoForm todosUpdater={this.todosUpdater} />
+        <TodoList todos={this.state.todos} todosUpdater={this.todosUpdater} />
       </div>
     );
   }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { destroy } from '../api';
 
 class TodoItem extends React.Component {
   render() {
-    // this.props から TodoItem に渡ってきた Props を取り出す
+    const id = this.props.id;
     const title = this.props.title;
     const text = this.props.text;
     const created_at = this.props.created_at;
+    const todosUpdater = this.props.todosUpdater;
     return (
       <tr>
-        {/* `{...}` で JSX 内に変数を展開する */}
         <td>{title}</td>
         <td>{text}</td>
         <td>{created_at}</td>
+        <td>
+          <button onClick={() => destroy(id).then(() => todosUpdater())}>
+            Delete
+          </button>
+        </td>
       </tr>
     );
   }
